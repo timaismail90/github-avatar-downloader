d129e0950b72839219c65a7fd89db6649ce128ea
Fatuma Ismail <timaismail90gmail.com>
2019-01-14 20:29:43 -0600

    download image function created and is working

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var secret = require('./secrets')
 var GITHUB_TOKEN = secret['GITHUB_TOKEN'];
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -22,7 +23,7 @@ function getAvatarUrl(err, res, body){
     var data = JSON.parse(body);
     var avatarUrl = data[0].avatar_url;
     data.forEach(function(user){
-      console.log(user.avatar_url);
+      downloadImageByURL(user.avatar_url, `avatars/${user.login}`)
     })
 
   }
@@ -41,5 +42,39 @@ getRepoContributors('jquery', 'jquery', getAvatarUrl);
 
 
 
-// ar request = require('request');
+  // ..
 
+
+function downloadImageByURL(url, filePath) {
+  fs.existsSync('avatars') || fs.mkdirSync('avatars');
+  request.get(url)               // Note 1
+        .pipe(fs.createWriteStream(filePath));
+
+       console.log('Download completed')
+
+
+  // ...
+}
+
+ downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg")
+
+
+// var request = require('request');
+
+
+// request.get('url')               // Note 1
+//        .on('error', function (err) {                                   // Note 2
+//          throw err;
+//        })
+//        .on('response', function (response) {                           // Note 3
+//          console.log('Response Status Code: ', response.statusCode);
+//        })
+//        .on('end', function(){
+
+//        })
+
+//        .pipe(fs.createWriteStream('./download.html'));
+
+//        console.log('Download completed')
+
+//                   // Note 4
